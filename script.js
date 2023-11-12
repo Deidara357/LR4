da4d484dcc63d40ebd95b1371710e35cb7a637e0
Deidara357 <deidara357357@gmail.com>
2023-11-12 23:11:55 +0200

    Task2 Completed

--- script.js
+++ script.js
@@ -9,18 +9,44 @@ function changeColor(elementId) {
             element.style.color = 'black';
         } else {
             element.style.backgroundColor = 'blue';
-            element.style.color = 'white';
+            element.style.color = 'pink';
         }
         element8Clicked = !element8Clicked;
     } else if (elementId === 'element9') {
         const element = document.querySelector('#element9');
         if (element9Clicked) {
             element.style.backgroundColor = 'blue';
-            element.style.color = 'white';
+            element.style.color = 'pink';
         } else {
             element.style.backgroundColor = 'green';
             element.style.color = 'black';
         }
         element9Clicked = !element9Clicked;
+    }
+}
+
+function addImage() {
+    var imageUrl = prompt('Введіть URL зображення:');
+    if (imageUrl) {
+      document.getElementById('main-image').src = imageUrl;
+    }
+}
+  
+function zoomIn() {
+    var image = document.getElementById('main-image');
+    var currentWidth = image.width;
+    image.style.width = (currentWidth * 1.2) + 'px';
+}
+  
+function zoomOut() {
+    var image = document.getElementById('main-image');
+    var currentWidth = image.width;
+    image.style.width = (currentWidth / 1.2) + 'px';
+}
+  
+function deleteImage() {
+    var confirmation = confirm('Ви впевнені, що хочете видалити зображення?');
+    if (confirmation) {
+      document.getElementById('main-image').src = '';
     }
 }
